ee26b7921531d3eb5ddd353cfe57cebf04c13f95
iceiceiceiceiceice <31066905+iceiceiceiceiceice@users.noreply.github.com>
2018-04-23 18:36:19 +0700

    quan ngu vl
    ngu that

--- soa2/src/Component/Student/Student.js
+++ soa2/src/Component/Student/Student.js
@@ -47,7 +47,17 @@ class Student extends Component{
     }
     // Lay form diem ren luyen
     createForm(){
-
+        ReactDOM.render(
+            <Student 
+                username={this.state.username}
+                islogin={this.state.islogin}
+                role={this.state.role}
+                token={this.props.token}
+                username= {this.props.username}
+                userID= {this.props.userID}
+            />,
+            document.getElementById("root")
+        )
     }
     handelName(event){
         this.setState({
@@ -72,6 +82,15 @@ class Student extends Component{
     SubmitPoint(event){
 
     }
+    GetForm(event){
+        axios
+            .get("https://training-point.herokuapp.com/get_form")
+            .then(Response =>{
+                this.setState({
+                    
+                });
+            })
+    }
     render(){
         return (
             <div className="w3-light-grey">
@@ -80,8 +99,9 @@ class Student extends Component{
             
             <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={()=> this.openMenu()}> &nbsp;Menu</button>
            
-            <span className="w3-bar-item w3-right"><button className="btn btn-lg btn-primary btn-block" onClick = {(e)=>this.Logout(e)}>Đăng xuất</button></span>
-            <span className="w3-bar-item w3-right"><button className="btn btn-lg btn-primary btn-block" onClick = {(e)=>this.createForm(e)}>Tạo đơn</button></span>
+            <span className="w3-bar-item w3-right"><button className="btn btn-primary btn-block" onClick = {(e)=>this.Logout(e)}>Đăng xuất</button></span>
+            <span className="w3-bar-item w3-right"><button className="btn btn-primary btn-block" onClick = {(e)=>this.createForm(e)}>Tạo đơn</button></span>
+            <span className="w3-bar-item w3-right"><button className="btn btn-primary btn-block" onClick = {(e)=>this.GetForm(e)}>Xem các đơn</button></span>
             </div>
             {/*Side bar, dung do trong man hinh */}
             <nav className="w3-sidebar w3-collapse w3-grey w3-animate-left" style={{zIndex: 3, width: 300}} id="mySidebar"><br />
@@ -93,15 +113,16 @@ class Student extends Component{
             </div>
             </nav>
             
-            <div className="w3-overlay w3-hide-large w3-animate-opacity" onClick={() => this.closeMenu()} style={{cursor: 'pointer'}} title="close side menu" id="myOverlay" />
+            <div className="w3-overlay w3-hide-large w3-animate-opacity" id="main" style={{cursor: 'pointer'}} title="close side menu" id="myOverlay" />
         
-            <div className="w3-main w3-light-grey" style={{marginLeft: 300, marginTop: 43}}>
+            <div className="w3-main w3-white" style={{marginLeft: 300, marginTop: 43}}>
 
             <header className="w3-container" style={{paddingTop: 22}}>
                 <h5><b><i className="fa fa-dashboard" /> Thông báo điểm rèn luyện</b></h5>
             </header>
             {/*Phan chinh cua trang web */}
-            <table class="table table-condensed table-bordered table-striped">
+            <table className="table table-condensed table-bordered table-striped">
+                
                 <tbody>
                     <tr>
                         <td>STT</td>
@@ -117,9 +138,10 @@ class Student extends Component{
                         <td><input type="text" onChange={(e)=> this.handelPoint1(e)}></input></td>
                         <td><input type="text" onChange={(e)=> this.handelPoint2(e)}></input></td>
                         <td><input type="text" onChange={(e)=> this.handelPoint3(e)}></input></td>
-                        <td><button type="submit" className="btn btn-lg btn-primary btn-block" onClick={(e) => this.SubmitPoint(e)}>Gửi</button></td>
+                        <td><button type="submit" className="btn btn-primary" onClick={(e) => this.SubmitPoint(e)}>Gửi</button></td>
                     </tr>
                 </tbody>
+
             </table>
          </div>
         </div>
